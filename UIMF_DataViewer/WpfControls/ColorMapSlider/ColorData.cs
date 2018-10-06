ecf1ccb9e4c8758f2cc8842060fae441c063f69f
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2018-10-06 13:06:42 -0700

    Add ColorMapSlider and GrayScaleSlider WPF implementations

---
+++ UIMF_DataViewer/WpfControls/ColorMapSlider/ColorData.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media;
+using ReactiveUI;
+
+namespace UIMF_DataViewer.WpfControls.ColorMapSlider
+{
+    public class ColorData : ReactiveObject
+    {
+        private double position;
+
+        public Color Color { get; }
+        public int Order { get; }
+
+        public double Position
+        {
+            get => position;
+            set => this.RaiseAndSetIfChanged(ref position, value);
+        }
+
+        public Brush Brush { get; }
+
+        public ColorData(Color color, int order, double position = 0)
+        {
+            Color = color;
+            Order = order;
+            Position = position;
+            Brush = new SolidColorBrush(Color);
+        }
+
+        public override string ToString()
+        {
+            return $"{Color}: {Order}, {Position:F3}";
+        }
+    }
+}
---
+++ UIMF_DataViewer/WpfControls/ColorMapSlider/ColorThumb.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace UIMF_DataViewer.WpfControls.ColorMapSlider
+{
+    public class ColorThumb : Thumb
+    {
+        static ColorThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorThumb), new FrameworkPropertyMetadata(typeof(ColorThumb)));
+        }
+
+        public Brush ThumbFill
+        {
+            get { return (Brush)GetValue(ThumbFillProperty); }
+            set { SetValue(ThumbFillProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ThumbFill.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ThumbFillProperty =
+            DependencyProperty.Register("ThumbFill", typeof(Brush), typeof(ColorThumb), new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+    }
+}
